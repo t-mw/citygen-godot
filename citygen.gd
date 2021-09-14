df6f067eeeb0f5a10f87c12ee0e88779128d04c3
Tobias Mansfield-Williams <contact@tmwhere.com>
2021-09-14 17:06:28 +0200

    Generate buildings

--- citygen.gd
+++ citygen.gd
@@ -17,7 +17,11 @@ const HIGHWAY_BRANCH_POPULATION_THRESHOLD := 0.1
 const NORMAL_BRANCH_POPULATION_THRESHOLD := 0.1
 const NORMAL_BRANCH_TIME_DELAY_FROM_HIGHWAY := 5
 const SEGMENT_COUNT_LIMIT := 2000
-const ROAD_SNAP_DISTANCE := 50
+const ROAD_SNAP_DISTANCE := 50 # world units
+
+const BUILDING_COUNT_PER_SEGMENT := 10
+const MAX_BUILDING_DISTANCE_FROM_SEGMENT := 400.0 # world units
+const BUILDING_PLACEMENT_LOOP_LIMIT := 3
 
 func random_branch_angle() -> float:
     return Math.random_angle(BRANCH_ANGLE_DEVIATION)
@@ -32,6 +36,7 @@ onready var physics_space := get_world_2d().direct_space_state
 onready var physics_space_rid := get_world_2d().space
 
 var segments = []
+var buildings = []
 
 func _ready():
     randomize()
@@ -43,11 +48,14 @@ func _ready():
     noise.persistence = 0.2
 
     segments = generate_segments()
+    buildings = generate_buildings(segments)
 
 func _draw():
     for segment in segments:
         var width = HIGHWAY_SEGMENT_WIDTH if segment.metadata.highway else DEFAULT_SEGMENT_WIDTH
         draw_line(segment.r_start, segment.r_end, Color.black, width, true)
+    for building in buildings:
+        draw_colored_polygon(building.generate_corners(), Color.black, [], null, null, true)
 
 func _process(delta):
     var query_shape = RectangleShape2D.new()
@@ -469,6 +477,116 @@ class SegmentMetadata:
         clone.highway = self.highway
         clone.severed = self.severed
         return clone
+
+const SEGMENT_STEP := 10
+func generate_buildings(segments: Array) -> Array:
+    var buildings = []
+
+    for i in range(0, len(segments), SEGMENT_STEP):
+        var segment: Segment = segments[i]
+
+        for _b in range(0, BUILDING_COUNT_PER_SEGMENT):
+            var random_angle = randf() * 360.0
+            var random_radius = randf() * MAX_BUILDING_DISTANCE_FROM_SEGMENT
+
+            var building = Building.new()
+            building.center = (segment.r_start + segment.r_end) * 0.5
+            building.center.x += random_radius * sin(deg2rad(random_angle))
+            building.center.y += random_radius * cos(deg2rad(random_angle))
+            building.direction = segment.direction
+            building.aspect_ratio = rand_range(0.5, 2.0)
+            building.diagonal = rand_range(80.0, 150.0)
+
+            var permit_building = false
+            var query = Physics2DShapeQueryParameters.new()
+            query.collide_with_bodies = false
+            query.collide_with_areas = true
+            for placement_i in range(0, BUILDING_PLACEMENT_LOOP_LIMIT):
+                query.shape_rid = building.create_physics_shape()
+                var matches = []
+                var query_results = physics_space.collide_shape(query)
+
+                var is_final_placement_iteration = placement_i == BUILDING_PLACEMENT_LOOP_LIMIT - 1
+                if len(query_results) == 0:
+                    permit_building = true
+                    break
+                elif is_final_placement_iteration:
+                    # there is no point checking the query results if we are on the final iteration
+                    # and there are still collisions
+                    break
+
+                for result in query_results:
+                    # move building away from collision
+                    building.center += (building.center - result)
+                building.destroy_physics_shape()
+
+            if permit_building:
+                building.attach_to_physics_space(physics_space_rid)
+                buildings.append(building)
+
+    return buildings
+
+class Building extends Object:
+    var physics_shape: RID
+    var physics_area: RID
+
+    var center: Vector2
+    var direction: float
+    var aspect_ratio: float
+    var diagonal: float
+    var corners: PoolVector2Array
+
+    func _notification(n):
+        if n == NOTIFICATION_PREDELETE:
+            self.detach_from_physics_space()
+
+    func create_physics_shape() -> RID:
+        if self.physics_shape.get_id() == 0:
+            self.physics_shape = Physics2DServer.convex_polygon_shape_create()
+            Physics2DServer.shape_set_data(self.physics_shape, PoolVector2Array(self.generate_corners()))
+        return self.physics_shape
+
+    func destroy_physics_shape():
+        if self.physics_shape.get_id() != 0:
+            Physics2DServer.free_rid(self.physics_shape)
+            self.physics_shape = RID()
+
+    func attach_to_physics_space(physics_space_rid: RID):
+        assert(physics_space_rid.get_id() != 0)
+        self.physics_area = Physics2DServer.area_create()
+        Physics2DServer.area_attach_object_instance_id(self.physics_area, get_instance_id())
+        Physics2DServer.area_set_monitorable(self.physics_area, false)
+        Physics2DServer.area_add_shape(self.physics_area, self.create_physics_shape())
+        Physics2DServer.area_set_space(self.physics_area, physics_space_rid)
+
+    func detach_from_physics_space():
+        if self.physics_area.get_id() != 0:
+            Physics2DServer.free_rid(self.physics_area)
+            self.physics_area = RID()
+        self.destroy_physics_shape()
+
+    func generate_corners() -> PoolVector2Array:
+        if len(self.corners) == 0:
+            var aspect_degrees = rad2deg(atan(self.aspect_ratio))
+            self.corners = PoolVector2Array([
+                Vector2(
+                    self.center.x + self.diagonal * sin(deg2rad(+aspect_degrees + self.direction)),
+                    self.center.y + self.diagonal * cos(deg2rad(+aspect_degrees + self.direction))
+                ),
+                Vector2(
+                    self.center.x + self.diagonal * sin(deg2rad(-aspect_degrees + self.direction)),
+                    self.center.y + self.diagonal * cos(deg2rad(-aspect_degrees + self.direction))
+                ),
+                Vector2(
+                    self.center.x + self.diagonal * sin(deg2rad(180 + aspect_degrees + self.direction)),
+                    self.center.y + self.diagonal * cos(deg2rad(180 + aspect_degrees + self.direction))
+                ),
+                Vector2(
+                    self.center.x + self.diagonal * sin(deg2rad(180 - aspect_degrees + self.direction)),
+                    self.center.y + self.diagonal * cos(deg2rad(180 - aspect_degrees + self.direction))
+                )
+            ])
+        return self.corners
 
 class Math:
     static func random_angle(limit: float) -> float:
